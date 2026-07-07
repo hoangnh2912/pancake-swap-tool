ded02662fa42d3c95db281ecab0dfb670ebc7924
haihoang646n <haihoang646n@prestigein.com>
2026-07-07 23:29:11 +0700

    revert: GAS_PRICE về 0.05 gwei (50M wei)

--- src/utils/automationService.ts
+++ src/utils/automationService.ts
@@ -3,7 +3,7 @@ import { ERC20_ABI, ROUTER_PANCAKE_V2_ABI } from './abi'
 import { buildProvider } from './buildProvider'
 import zenStackFunction from './zenstack-function'
 
-const GAS_PRICE = ethers.utils.parseUnits('1', 'gwei')
+const GAS_PRICE = ethers.BigNumber.from(50_000_000)
 
 const CHAIN_CONFIG: Record<number, { router: string; factory: string; wbnb: string }> = {
     56: {
